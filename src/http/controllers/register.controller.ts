d714ec9bf2567db95025e4d617b5262a153616ff
Gabriel Lemos <glemoss@yahoo.com>
2024-07-11 15:28:52 -0300

    feat: register user - register controller - register service - register route

---
+++ src/http/controllers/register.controller.ts
@@ -0,0 +1,25 @@
+import { z } from 'zod'
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { registerService } from '@/services/register.service'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(8),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  try {
+    await registerService({
+      name,
+      email,
+      password,
+    })
+  } catch (err) {
+    return reply.status(409).send()
+  }
+
+  return reply.status(201).send()
+}
